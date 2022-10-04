55e1405d8b1bbc1883dc7057795cacedf9c4390c
MakarovOleg1988 <makarov.oleg1988@yandex.ru>
2022-10-04 13:11:36 +0300

    Добавьте файлы проекта.

---
+++ Lesson16/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson16
+{
+
+    public class RareType
+    {
+        public void Choosetype()
+        {
+            Dictionary<int, string> _rareWeapon = new Dictionary<int, string>();
+            _rareWeapon.Add(1, "Simple");
+            _rareWeapon.Add(2, "Legendary");
+        }
+    }
+
+    public class BaseWeapon : RareType
+    {
+        protected float _power;
+        protected float _range;
+
+        public BaseWeapon()
+        {
+            _self = this;
+        }
+
+        public static BaseWeapon _self;
+
+        public static string GetNameLastObject()
+        {
+            return _self.GetType().Name;
+        }
+
+        static BaseWeapon()
+        {
+            Console.WriteLine("BaseWeapon");
+        }
+    }
+    class CommonBow: BaseWeapon
+    {
+        public static string GetName()
+        {
+            return "Weapon";
+        }
+
+        static CommonBow()
+        {
+            Console.WriteLine("CommonBow");
+        }
+    }
+
+    class Axe: BaseWeapon
+    { 
+    
+    }
+}
---
+++ Lesson16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson16
+{
+    class Program
+    {
+
+        public static int _value;
+        public static int _valueProp { get; set; }
+
+        static void Main(string[] args)
+        {
+            CommonBow _bow = new CommonBow();
+            Console.WriteLine(BaseWeapon.GetNameLastObject());
+
+            Axe _axe = new Axe();
+            Console.WriteLine(BaseWeapon.GetNameLastObject());
+            
+            var r = CommonBow.GetName();
+
+            _value = 10;
+            _valueProp = 5;
+        }
+    }
+}
